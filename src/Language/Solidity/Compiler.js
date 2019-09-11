4ea5efdf9d027c01c713781cb70d28b549d227f0
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2019-09-11 10:04:12 -0400

    support loadRemoteVersion solc call

--- src/Language/Solidity/Compiler.js
+++ src/Language/Solidity/Compiler.js
@@ -1,6 +1,12 @@
 "use strict";
 
-var solc = require('solc');
+const solc = require('solc');
+
+exports.defaultCompiler = solc;
+
+exports.version = function(solc) {
+  return solc.version;
+}
 
 exports.callbackSuccess = function (contents) {
   return { "contents": contents }
@@ -10,10 +16,26 @@ exports.callbackFailure = function (error) {
   return { "error": error }
 };
 
+exports._loadRemoteVersion = function(version) {
+  return function (onError, onSuccess) {
+    var cancel = solc.loadRemoteVersion(version, function(err, solcSnapshot) {
+      if (err) {
+        onError(err);
+      } else {
+        onSuccess(solcSnapshot);
+      }
+    });
+    return function(cancelError, onCancelerError, onCancelerSuccess) {
+      cancel();
+      onCancelerSuccess();
+    };
+  }
+};
+
 exports._compile = function (input, readCallback) {
   return function() {
-    return solc.compile(input, function(requestedFile) {
+    return JSON.parse(solc.compile(JSON.stringify(input), function(requestedFile) {
       return readCallback(requestedFile)();
-    });
+    }));
   }
 };
--- src/Language/Solidity/Compiler.purs
+++ src/Language/Solidity/Compiler.purs
@@ -1,35 +1,49 @@
 module Language.Solidity.Compiler
-  ( compile
+  ( SolidityCompiler
+  , defaultCompiler
+  , compile
+  , loadRemoteVersion
+  , version
   ) where
 
 import Prelude
 
+import Data.Argonaut (Json, encodeJson)
 import Data.Argonaut as A
-import Data.Argonaut.Parser as AP
-import Data.Either (Either(..))
-import Data.Function.Uncurried (Fn2, runFn2)
+import Data.Either (Either, either)
+import Data.Function.Uncurried (Fn3, runFn3)
 import Effect (Effect)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 import Effect.Class (class MonadEffect, liftEffect)
 import Language.Solidity.Compiler.Types (CompilerInput, CompilerOutput)
 import Node.Path (FilePath)
 
 foreign import data SolcReadFileCallbackResult :: Type
+foreign import data SolidityCompiler :: Type
 foreign import callbackSuccess :: String -> SolcReadFileCallbackResult
 foreign import callbackFailure :: String -> SolcReadFileCallbackResult
-foreign import _compile :: Fn2 String (FilePath -> Effect SolcReadFileCallbackResult) (Effect String)
+foreign import defaultCompiler :: SolidityCompiler
+foreign import version :: SolidityCompiler -> String
+foreign import _loadRemoteVersion :: String -> EffectFnAff SolidityCompiler
+foreign import _compile :: Fn3 SolidityCompiler Json (FilePath -> Effect SolcReadFileCallbackResult) (Effect Json)
 
-compile :: forall m.
-           MonadEffect m
-        => CompilerInput
-        -> (FilePath -> Effect (Either String String))
-        -> m (Either String CompilerOutput)
-compile input readFile = do
-  compilerOutput <- liftEffect $ runFn2 _compile inputJson liftedCallback
-  pure (AP.jsonParser compilerOutput >>= A.decodeJson)
+compile
+  :: forall m
+   . MonadEffect m
+  => SolidityCompiler
+  -> CompilerInput
+  -> (FilePath -> Effect (Either String String))
+  -> m (Either String CompilerOutput)
+compile solc input readFile = liftEffect $
+  A.decodeJson <$> runFn3 _compile solc (encodeJson input) liftedCallback
 
   where inputJson      = A.stringify (A.encodeJson input)
-        liftedCallback path = do
-          callbackResult <- readFile path
-          pure $ case callbackResult of
-            Left err  -> callbackFailure err
-            Right res -> callbackSuccess res
+        liftedCallback = map (either callbackFailure callbackSuccess) <<< readFile
+
+loadRemoteVersion
+  :: forall m
+   . MonadAff m
+  => String
+  -> m SolidityCompiler
+loadRemoteVersion = liftAff <<< fromEffectFnAff <<< _loadRemoteVersion
